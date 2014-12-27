f20fceb8f27a5f5da9453a2c812f1042f514edaf
Kirill Ignatiev <kirill.ignatiev@gmail.com>
2014-12-27 01:57:43 -0500

    Initial commit.

---
+++ test.el
@@ -0,0 +1,17 @@
+(defun
+    function
+         (arglist elt1
+                  elt2
+                     elt3)
+  "docstring"
+    (something something
+                      something
+           something)
+  (another
+      thing
+               again))
+
+;; Local Variables:
+;; electric-indent-mode: nil
+;; eval: (when (fboundp (quote flycheck-mode)) (flycheck-mode -1))
+;; End:
